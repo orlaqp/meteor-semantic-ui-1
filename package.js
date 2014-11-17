c4b7abdc44676e00e518094851b59be650636096
Orlando Quero <orlaqp@gmail.com>
2014-11-16 22:35:30 -0500

    include fonts and images

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'orlaqp:semantic-ui',
   summary: ' Meteor Package for Semantic UI',
-  version: '1.0.0',
+  version: '1.0.1',
   git: 'https://github.com/orlaqp/meteor-semantic-ui'
 });
 
@@ -12,12 +12,22 @@ Package.onUse(function(api) {
 
   var path = Npm.require('path');
   var assetPath = path.join('lib/semantic-ui/dist/');
+  var fontsPath = path.join(assetPath, 'themes/default/assets/fonts/');
+  var imagesPath = path.join(assetPath, 'themes/default/assets/images/');
 
   console.log(assetPath);
 
   var assetFiles = [
     assetPath + 'semantic.js',
     assetPath + 'semantic.css',
+    // fonts/icons
+    fontsPath + 'icons.eot',
+    assetPath + 'icons.otf',
+    assetPath + 'icons.svg',
+    assetPath + 'icons.ttf',
+    assetPath + 'icons.woff',
+    // images
+    imagesPath + 'flags.png',
   ];
 
   api.add_files(assetFiles, 'client');
